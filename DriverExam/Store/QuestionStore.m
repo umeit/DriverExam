d47d1d2f13c206c931fab26b630d1aa50cced504
沈 湛 <lalunazhan@shenzhandeMBP.lan>
2015-01-21 11:51:28 +0800

    init2

--- DriverExam/Store/QuestionStore.m
+++ DriverExam/Store/QuestionStore.m
@@ -7,12 +7,19 @@
 //
 
 #import "QuestionStore.h"
+#import "QuestionBase.h"
+
+#import "FMDatabase.h"
+
+#define CURRENT_QUESTION_INDEX @"CurrentQuestionIndex"
+
+#define QUESTION_CONTENT @"name"
+
+#define USER_DEFAULTS [NSUserDefaults standardUserDefaults]
 
 @interface QuestionStore ()
 
-
-
-
+@property (strong, nonatomic) FMDatabase *dataBase;
 
 @end
 
@@ -34,9 +41,41 @@ static QuestionStore *exercisesStore = nil;
 {
     self = [super init];
     if (self) {
-        
+        _dataBase = [FMDatabase databaseWithPath:[self dbPath]];
+        [USER_DEFAULTS registerDefaults:@{CURRENT_QUESTION_INDEX: @1}];
     }
     return self;
+}
+
+- (QuestionBase *)nextQuestion
+{
+    NSInteger currentQuestionIndex = [USER_DEFAULTS integerForKey:CURRENT_QUESTION_INDEX];
+    currentQuestionIndex++;
+    
+    if (![self.dataBase open]) {
+        return nil;
+    }
+    
+    FMResultSet *result = [self.dataBase executeQuery:@"SELECT * FROM tbl_library_general WHERE id = ?",
+                           currentQuestionIndex];
+
+    QuestionBase *question = nil;
+    if ([result next]) {
+        question = [[QuestionBase alloc] init];
+        question.content = [result stringForColumn:QUESTION_CONTENT];
+    }
+    [USER_DEFAULTS setObject:@(currentQuestionIndex) forKey:CURRENT_QUESTION_INDEX];
+    [self.dataBase close];
+    return question;
+}
+
+- (NSString *)dbPath
+{
+    static NSString *dbPath;
+    if (!dbPath) {
+        dbPath = [[NSBundle mainBundle] pathForResource:@"km1" ofType:@"db"];
+    }
+    return dbPath;
 }
 
 @end

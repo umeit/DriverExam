d057160f6706d9a07d45bc7cbc9f3363015d42a3
Liu Feng <gumeit@gmail.com>
2015-03-03 10:37:01 +0800

    update

--- DriverExam/Controller/ADWebViewController.m
+++ DriverExam/Controller/ADWebViewController.m
@@ -17,44 +17,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    NSURL *url =[NSURL URLWithString:@"http://www.baidu.com"];
+    
+    NSURL *url =[NSURL URLWithString:@"http://www.youche.com"];
     NSURLRequest *request =[NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
-    self.webView.delegate = self;
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-- (void)webViewDidStartLoad:(UIWebView *)webView
-{
-    NSLog(@"loding....");
-}
-
-- (void)webViewDidFinishLoad:(UIWebView *)webView
-{
-    NSLog(@"load finish");
-}
-
-- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
-{
-    NSLog(@"local error");
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
